439170e514d6f2cc9e46ac6879a82c6c1c84965f
patrike <vuncano18@gmail.com>
2023-09-26 15:16:08 -0300

    spelling errors

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,8 +8,8 @@ else
   echo "no pacman updates"
 fi
 
-[ -d $HOME/.config/yay ] && echo " "; read -p"do you want to update yay packages? [y/n] " yay_yes
+[ -d $HOME/.config/yay ] && echo " "; read -p"do you want to update yay packages? [y/N] " yay_yes
 [ ${yay_yes} = y ] && yay -Sua || echo " "; echo "not possible to update yay packages"
 
-[ -d $HOME/.config/yay ] && echo " "; read -p"do you want to see the arch linux news page? [y/n] " news_yes
+[ -d $HOME/.config/yay ] && echo " "; read -p"do you want to see the arch linux news page? [y/N] " news_yes
 [ ${news_yes} = y ] && yay -Pww
