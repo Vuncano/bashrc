0e5e42fbd2965704d316ca5c0e9458a6a0c76ee6
vuncano <vuncano18@gmail.com>
2024-01-14 00:41:17 -0300

    screenshot script v1

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,14 +2,22 @@
 
 parameter="$1"
 dir="$2"
-temp_png="$HOME/Pictures/screen_$(date +'%F_%H-%M-%S').png"
+temp_png="$HOME/Pictures/Screenshots/screen_$(date +'%F_%H-%M-%S').png"
 
 case "$parameter" in
   "clipboard"|"clip")
     scrot "$temp_png" && xclip -selection clipboard -t image/png "$temp_png" && rm -fr "$temp_png"
+    exit 0
     ;;
   "save")
     scrot "$dir"
+    exit 0
+    ;;
+  "")
+    scrot "$temp_png"
+    exit 0
     ;;
   *)
 esac
+
+exit 1
